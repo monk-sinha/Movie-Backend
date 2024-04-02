32ab0c93c95625647fc444557083d3d50ebc14d4
monk-sinha <monk@monkstation>
2024-04-03 00:57:38 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,29 @@
+const express = require('express')
+const app = express();
+
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const PORT = 8000;
+const cookieParser = require('cookie-parser');
+
+const authRoutes = require('./Routes/Auth');
+const adminRoutes = require('./Routes/Admin')
+const movieRoutes = require('./Routes/Movie')
+
+require('dotenv').config();
+require('./database')
+
+app.use(express.json());
+app.use("/auth", authRoutes)
+app.use("/admin", adminRoutes)
+app.use("/movie", movieRoutes)
+
+app.get("/",(req, res) => {
+    res.send("API works")
+})
+
+app.use(cookieParser())
+
+app.listen(PORT, ()=>{
+    console.log("Server works");
+});
